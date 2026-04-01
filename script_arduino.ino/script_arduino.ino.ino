10f4ddb45383d92bcabca31ce95f6f477beba05b
vitorluzsantos <vitor.lsantos@sptech.school>
2026-04-01 10:09:13 -0300

    feat: arquivo de script arduino e script de criação do usuario

---
+++ script_arduino.ino/script_arduino.ino.ino
@@ -0,0 +1,22 @@
+//sensor HC-SR04
+#include "Ultrasonic.h"
+
+int pinoTrigger = 12;
+int pinoEcho = 13;
+HC_SR04 = sensor(pinoTrigger, pinoEcho);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  float distancia = sensor.distance();
+
+  if (distancia > 0 && distancia < 5) {
+    Serial.println(1);
+  }
+  
+    delay(1000);
+}
